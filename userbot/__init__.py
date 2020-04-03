53c5f93fe19a96c585b4501fa8608aa34ea6cc8d
jefa2231 <39442318+jefa2231@users.noreply.github.com>
2020-04-03 23:26:07 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -79,7 +79,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL and BRANCH for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/MoveAngel/One4uBot.git")
+    "https://github.com/jefa2231/SenturyUbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "sql-extended")
 
